5063c7ced85ff2eb19c050cbc523f617dcf7bc44
Phaneendhra <phani4842@gmail.com>
2022-07-25 18:40:19 +0530

    Java Script Practice

--- functions.js
+++ functions.js
@@ -18,3 +18,22 @@ b();
 function (){
 
 }*/ // Thorws an error (requires name)
+
+// arrow functions 1
+let add = function(num1,num2){
+    return num1+num2;
+}
+let result = add(5,6)
+console.log(result)
+
+// arrow functions 2
+let add_new = (num1,num2)=>{
+    return num1+num2;
+}
+let res = add_new(10,5)
+console.log(res)
+
+// arrow functions 3
+let add_nums = (num1,num2)=>num1+num2;      // if there is only one statement no need braces and return statement
+let res_new = add_nums(10,25)
+console.log(res_new)
