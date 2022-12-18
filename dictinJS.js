a117430fb1f1124c124285989965dee8797e292f
Phaneendhra <phani4842@gmail.com>
2022-12-18 19:49:15 +0530

    JavaScript Practice

---
+++ dictinJS.js
@@ -0,0 +1,11 @@
+var user = {
+    "name" :"Phani",
+    "age" : 22,
+    "gender": "Male"
+}
+
+console.log(user["name"]);
+console.log(user.name);     // either way we can access a key
+user["lastname"] = "chinta"
+
+console.log(user+"  "+typeof(user))
